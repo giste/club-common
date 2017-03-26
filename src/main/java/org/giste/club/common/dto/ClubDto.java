767e1f44114ea747e48ab7752a3e919f65fb523b
giste <giste.trappiste@gmail.com>
2017-03-26 11:38:43 +0200

    Initial commit.

---
+++ src/main/java/org/giste/club/common/dto/ClubDto.java
@@ -0,0 +1,81 @@
+package org.giste.club.common.dto;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+/**
+ * DTO object form transfer values between client and server. It has the
+ * following fields.
+ * <ul>
+ * <li>id: Unique identifier of club.
+ * <li>name: Complete name of the club. Size must be between 3 and 64
+ * characters.
+ * <li>acronym: Short name for the club. Size must be between 3 and 5
+ * characters. Has to be formed by capital letters (A-Z) and numbers (0-9).
+ * <li>enabled: Indicates if the club is enabled in the application (can be
+ * accessed) or not.
+ * </ul>
+ * 
+ * @author Giste
+ */
+public class ClubDto {
+
+	private Long id = 0L;
+
+	@NotNull
+	@Size(min = 3, max = 64)
+	private String name;
+
+	@NotNull
+	@Pattern(regexp = "[A-Z0-9]{3,5}")
+	private String acronym;
+
+	private boolean enabled = false;
+
+	public ClubDto() {
+
+	}
+
+	public ClubDto(Long id, String name, String acronym, Boolean enabled) {
+		this.id = id;
+		this.name = name;
+		this.setAcronym(acronym);
+		this.enabled = enabled;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAcronym() {
+		return acronym;
+	}
+
+	public void setAcronym(String acronym) {
+		if (acronym != null) {
+			this.acronym = acronym.toUpperCase();
+		}
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+}
---
+++ src/test/java/org/giste/club/common/dto/ClubDtoTest.java
@@ -0,0 +1,119 @@
+package org.giste.club.common.dto;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.giste.club.common.dto.ClubDto;
+import org.giste.util.StringUtil;
+
+public class ClubDtoTest {
+	private final static Long ID_OK = 0L;
+	private final static String NAME_OK = "Club deportivo 1";
+	private final static String ACRONYM_OK = "CLUB1";
+
+	private static Validator validator;
+
+	@BeforeClass
+	public static void setUp() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
+
+	@Test
+	public void clubIsValid() {
+		ClubDto club = new ClubDto(ID_OK, NAME_OK, ACRONYM_OK, false);
+
+		Set<ConstraintViolation<ClubDto>> violations = validator.validate(club);
+		assertEquals(0, violations.size());
+	}
+
+	@Test
+	public void nameTooLong() {
+		ClubDto club = new ClubDto(ID_OK, StringUtil.ofLength(65), ACRONYM_OK, false);
+
+		Set<ConstraintViolation<ClubDto>> violations = validator.validate(club);
+
+		assertEquals(1, violations.size());
+		ConstraintViolation<ClubDto> violation = violations.iterator().next();
+		assertEquals("name", violation.getPropertyPath().toString());
+		assertEquals("{javax.validation.constraints.Size.message}", violation.getMessageTemplate());
+	}
+
+	@Test
+	public void nameTooShort() {
+		ClubDto club = new ClubDto(ID_OK, StringUtil.ofLength(2), ACRONYM_OK, false);
+
+		Set<ConstraintViolation<ClubDto>> violations = validator.validate(club);
+
+		assertEquals(1, violations.size());
+		ConstraintViolation<ClubDto> violation = violations.iterator().next();
+		assertEquals("name", violation.getPropertyPath().toString());
+		assertEquals("{javax.validation.constraints.Size.message}", violation.getMessageTemplate());
+	}
+
+	@Test
+	public void parametersAreNull() {
+		ClubDto club = new ClubDto(1L, null, null, false);
+
+		Set<ConstraintViolation<ClubDto>> violations = validator.validate(club);
+
+		assertEquals(2, violations.size());
+		for (ConstraintViolation<ClubDto> violation : violations) {
+			assertEquals("{javax.validation.constraints.NotNull.message}", violation.getMessageTemplate());
+		}
+	}
+
+	@Test
+	public void acronymTooLong() {
+		ClubDto club = new ClubDto(ID_OK, NAME_OK, StringUtil.ofLength(6), false);
+
+		Set<ConstraintViolation<ClubDto>> violations = validator.validate(club);
+
+		assertEquals(1, violations.size());
+		ConstraintViolation<ClubDto> violation = violations.iterator().next();
+		assertEquals("acronym", violation.getPropertyPath().toString());
+		assertEquals("{javax.validation.constraints.Pattern.message}", violation.getMessageTemplate());
+	}
+
+	@Test
+	public void acronymTooShort() {
+		ClubDto club = new ClubDto(ID_OK, NAME_OK, StringUtil.ofLength(2), false);
+
+		Set<ConstraintViolation<ClubDto>> violations = validator.validate(club);
+
+		assertEquals(1, violations.size());
+		ConstraintViolation<ClubDto> violation = violations.iterator().next();
+		assertEquals("acronym", violation.getPropertyPath().toString());
+		assertEquals("{javax.validation.constraints.Pattern.message}", violation.getMessageTemplate());
+	}
+
+	@Test
+	public void acronymWithInvalidCharacters() {
+		ClubDto club = new ClubDto(ID_OK, NAME_OK, "AA_AA", false);
+
+		Set<ConstraintViolation<ClubDto>> violations = validator.validate(club);
+
+		assertEquals(1, violations.size());
+		ConstraintViolation<ClubDto> violation = violations.iterator().next();
+		assertEquals("acronym", violation.getPropertyPath().toString());
+		assertEquals("{javax.validation.constraints.Pattern.message}", violation.getMessageTemplate());
+	}
+	
+	@Test
+	public void acronymToUpperCase() {
+		ClubDto club = new ClubDto(ID_OK, NAME_OK, ACRONYM_OK.toLowerCase(), false);
+		
+		assertThat(club.getAcronym(), is(ACRONYM_OK));
+	}
+
+}
