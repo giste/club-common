10f3e9e0307da6748b0a72db0fd220ea78688d2a
Giste Trappiste <giste.trappiste@gmail.com>
2017-12-12 17:44:02 +0100

    Left only Season DTO.

--- src/main/java/org/giste/club/common/dto/SeasonDto.java
+++ src/main/java/org/giste/club/common/dto/SeasonDto.java
@@ -1,24 +1,20 @@
 package org.giste.club.common.dto;
 
-import java.io.Serializable;
-
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import org.giste.util.dto.BaseDto;
 
-public class SeasonDto extends BaseDto implements Serializable {
-
-	private static final long serialVersionUID = -7718666985037630483L;
+public class SeasonDto extends BaseDto {
 
 	@NotNull
 	@Min(2017)
 	private Integer year = java.time.LocalDate.now().getYear();
-	
+
 	public SeasonDto() {
-		
+
 	}
-	
+
 	/**
 	 * Creates a new season DTO.
 	 * 
@@ -41,9 +37,40 @@ public class SeasonDto extends BaseDto implements Serializable {
 	/**
 	 * Gets the name of the season based on the year it begins.
 	 * 
-	 * @return The name of the season. 
+	 * @return The name of the season.
 	 */
 	public String getName() {
 		return year + "-" + (year + 1);
 	}
+
+	@Override
+	public String toString() {
+		return String.format("SeasonDto [id=%s, year=%s]", id, year);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((year == null) ? 0 : year.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SeasonDto other = (SeasonDto) obj;
+		if (year == null) {
+			if (other.year != null)
+				return false;
+		} else if (!year.equals(other.year))
+			return false;
+		return true;
+	}
+
 }
